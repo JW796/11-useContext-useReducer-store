edeae65377e5ac2ba16da4b339a5891fdf107007
James Joseph <98562132+JW796@users.noreply.github.com>
2025-03-21 10:59:50 -0400

    WIP data setup + navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="basket">Basket</Link>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+  {
+    name: "",
+    price: 128.28,
+  },
+  {
+    name: "",
+    price: 79.48,
+  },
+  {
+    name: "",
+    price: 82.50,
+  },
+];
