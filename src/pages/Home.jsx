b0dc35f89151b380dba49f8d1145fb89192a55a4
James Joseph <98562132+JW796@users.noreply.github.com>
2025-03-24 11:59:03 -0400

    WIP 2 data setup + navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl text-center font-semibold mt-2 py-4">useContext + useReducer Store</h1>
+      <h2 className='text-4xl tracking-wide py-4 pl=8'>New In Stock</h2>
     </div>
   );
 }
