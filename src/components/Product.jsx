81a612b7cecc1d5ec5b64766c0ddd4e12be835bb
James Joseph <98562132+JW796@users.noreply.github.com>
2025-03-26 11:54:11 -0400

    WIP display products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,11 @@
-const Product = ({item}) => {
+const Product = ({ item }) => {
   return (
-    <div>
+    <div className="mx-auto px-8 border border-gray-200">
       <img src={item.image} />
+      <p className="text-xl">{image.name}</p>
+      <p className="text-lg">${item.price}</p>
     </div>
-  )
+  );
 }
 
 export default Product
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
-import { storeData } from '../Data';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { storeData } from "../Data";
 import Product from "../components/Product";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1 className="text-3xl text-center font-semibold mt-2 py-4">useContext + useReducer Store</h1>
+      <h1 className="text-3xl text-center font-semibold mt-2 py-4">
+        useContext + useReducer Store
+      </h1>
       <h2 className="text-4xl tracking-wide py-4 pl-8">New In Stock</h2>
-      {storeData.map((item, i ) => 
-        <Product 
-        key={i}
-        item={item}
-        />
-      )}
+      <div className="flex flex-col items-center gap-4 lg:grid lg:grid-cols-3">
+        {storeData.map((item, i) => (
+          <Product 
+            key={i} 
+            item={item} 
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
