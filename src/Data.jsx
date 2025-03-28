a4683daa481358b0934f1965ec5f98347fc7684d
James Joseph <98562132+JW796@users.noreply.github.com>
2025-03-27 20:24:03 -0400

    Products bug fix

---
+++ src/Data.jsx
@@ -0,0 +1,20 @@
+export const storeData = [
+  {
+    name: "Florence",
+    price: 128.28,
+    image:
+      "https://dam.nowoptics.net/transform/05610f34-32f7-41e6-928c-91c3a06bdbd6/2007891-IMPW-16-38_02"
+  },
+  // {
+  //   name: "Light Specs",
+  //   price: 79.48,
+  //   image:
+  //     "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpanthervision.com%2Fproducts%2Flightspecs-lp-led-reading-glasses&psig=AOvVaw3zATIc7D75IkIazApX7A_A&ust=1743000065976000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLikybK7pYwDFQAAAAAdAAAAABAK",
+  // },
+  // {
+  //   name: "Mikemo",
+  //   price: 118.95,
+  //   imgage:
+  //     "https://www.google.com/url?sa=i&url=https%3A%2F%2Fglassyeyewear.com%2Fproducts%2Fmikemo-prescription-glasses&psig=AOvVaw3zATIc7D75IkIazApX7A_A&ust=1743000065976000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLikybK7pYwDFQAAAAAdAAAAABAP",
+  // },
+];
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ const Product = ({ item }) => {
   return (
     <div className="mx-auto px-8 border border-gray-200">
       <img src={item.image} />
-      <p className="text-xl">{image.name}</p>
+      <p className="text-xl">{item.name}</p>
       <p className="text-lg">${item.price}</p>
     </div>
   );
