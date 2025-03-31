48604138dd01906239ff39b9a6d3e2f7f7f1c465
James Joseph <98562132+JW796@users.noreply.github.com>
2025-03-31 12:37:15 -0400

    Responsive styling update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,8 +2,8 @@ const Product = ({ item }) => {
   return (
     <div className="mx-auto px-8 border border-gray-200 rounded-lg">
       <img src={item.image} alt="glasses" />
-      <div>
-        <div className="flex flex-row items-center justify-between py-4">
+      <div className="flex flex-row items-center justify-between py-4">
+        <div>
           <p className="text-xl">{item.name} </p>
           <p className="text-lg">${item.price}</p>
         </div>
