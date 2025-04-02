be2a89a0633c2b7412eebdd15f14dc98df7deec2
James Joseph <98562132+JW796@users.noreply.github.com>
2025-04-02 12:59:07 -0400

    Add handleAdd to cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,13 @@
+import { useContext } from "react";
+import { StoreContext } from "../context-and-reducer/StoreContext";
+
 const Product = ({ item }) => {
+    const {addToBasket} = useContext(StoreContext);
+
+    const handleAdd = () => {
+      addToBasket(item)
+    }
+    
   return (
     <div className="mx-auto px-8 border border-gray-200 rounded-lg">
       <img src={item.image} alt="glasses" />
@@ -7,7 +16,9 @@ const Product = ({ item }) => {
           <p className="text-xl">{item.name} </p>
           <p className="text-lg">${item.price}</p>
         </div>
-        <button className="bg-indigo-500 text-white text-lg font-medium rounded-full p-4">
+        <button className="bg-indigo-500 text-white text-lg font-medium rounded-full p-4"
+        onClick={handleAdd}
+        >
           Add to Cart
         </button>
       </div>
